d743acdcbad6359924cb632fbd02ddbbe8514a16
Kralik N <kraaliknorbert@gmail.com>
2025-07-09 17:15:18 +0200

    enhance landing intro layout and typography

--- components/LandingIntro.jsx
+++ components/LandingIntro.jsx
@@ -13,11 +13,12 @@ import { Separator } from './ui/separator'
 
 const LandingIntro = () => {
     return (
-        <Card className="shadow-lg rounded-2xl p-3 sm:p-6 w-full sm:w-[680px] mb-16">
+        <Card className="shadow-lg rounded-2xl p-3 sm:p-6  w-full max-w-2xl mx-auto mb-10">
             <CardHeader className="p-2 sm:px-6">
                 <CardTitle>
                     <h1 className="text-2xl sm:text-4xl font-extrabold tracking-tight text-pretty">
-                        Hello, Králik Norbert vagyok,
+                        <span className="block">Hello,</span>{' '}
+                        <span className="block ">Králik Norbert vagyok,</span>
                     </h1>
                 </CardTitle>
                 <CardDescription >
@@ -28,14 +29,14 @@ const LandingIntro = () => {
                 <Separator className="" />
             </CardHeader>
             <CardContent className="text-pretty p-2 sm:px-6">
-                <p className="mb-4 text-foreground/80 dark:text-muted-foreground text-sm sm:text-base leading-relaxed">
+                <p className="mb-4 text-foreground/80 dark:text-muted-foreground text-[15px] sm:text-base leading-relaxed">
                     A webfejlesztés és a gépi tanulás világában mélyedek el, saját projekteken keresztül.
                     Frontenden <span className="font-semibold text-foreground">Next.js</span>-t, backenden <span className="font-semibold text-foreground">FastAPI</span>-t és <span className="font-semibold text-foreground">Python</span>-t használok – a rendszereket <span className="font-semibold text-foreground">Azure</span> cloud környezetben futtatom, <span className="font-semibold text-foreground">SQL</span> adatbázisokkal kiegészítve.
                 </p>
-                <p className="mb-4 text-foreground/80 dark:text-muted-foreground text-sm sm:text-base leading-relaxed">
+                <p className="mb-4 text-foreground/80 dark:text-muted-foreground text-[15px] sm:text-base leading-relaxed">
                     Ez az oldal a fejlődésem terepe – hamarosan megosztom első alkalmazásaimat, és folyamatosan frissítem új ötletekkel.
                 </p>
-                <p className="mb-2 text-foreground/80 dark:text-muted-foreground text-sm sm:text-base leading-relaxed">
+                <p className="mb-2 text-foreground/80 dark:text-muted-foreground text-[15px] sm:text-base leading-relaxed">
                     Van egy izgalmas terved, amihez jól jönne egy elkötelezett fejlesztő? Keress bátran!
                 </p>
             </CardContent>
